0f8cddbb129cd25d028ab42ab0ffec3637c53038
murad <kh4lidov@mail.ru>
2024-06-22 23:07:45 +0300

    set forceScheme 'https'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (App::isProduction()) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
@@ -19,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (App::isProduction()) {
+            URL::forceScheme('https');
+        }
     }
 }
