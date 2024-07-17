d4cb6ea08680c7320599b7116616555a457ccf5a
murad <kh4lidov@mail.ru>
2024-07-18 02:40:04 +0300

    set header as fixed

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -21,9 +21,68 @@ export default function Authenticated({ user, children }) {
 
     return (
         <div className="min-h-screen flex w-full bg-white text-slate-800">
+            <div className="flex bg-white fixed w-full h-16 items-center gap-4 border-b px-6 justify-end">
+                <div>
+                    <input
+                        className="w-full rounded-md border-0 py-1.5 pl-7 pr-20 ring-1 ring-inset ring-slate-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
+                        type="text"
+                        placeholder="Search"
+                    />
+                </div>
+                <div className="flex items-center gap-2">
+                    <div className="border-r px-4 hidden sm:block">
+                        <BellIcon className="h-6 w-6 cursor-pointer text-slate-700" />
+                    </div>
+                    <div className="ml-2 flex items-center justify-center gap-2">
+                        <div>
+                            <Dropdown>
+                                <Dropdown.Trigger>
+                                    <span className="inline-flex rounded-md">
+                                        <button
+                                            type="button"
+                                            className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                        >
+                                            {user.name}
+
+                                            <svg
+                                                className="ms-2 -me-0.5 h-4 w-4"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20"
+                                                fill="currentColor"
+                                            >
+                                                <path
+                                                    fillRule="evenodd"
+                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                    clipRule="evenodd"
+                                                />
+                                            </svg>
+                                        </button>
+                                    </span>
+                                </Dropdown.Trigger>
+
+                                <Dropdown.Content>
+                                    <Dropdown.Link
+                                        className="sm:hidden"
+                                        href={route("profile.edit")}
+                                    >
+                                        Settings
+                                    </Dropdown.Link>
+                                    <Dropdown.Link
+                                        href={route("logout")}
+                                        method="post"
+                                        as="button"
+                                    >
+                                        Log Out
+                                    </Dropdown.Link>
+                                </Dropdown.Content>
+                            </Dropdown>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div
                 className={classNames(
-                    "hidden sm:flex flex-col justify-between border-r pb-4 transition-all duration-300",
+                    "hidden z-10 bg-white sm:flex flex-col justify-between border-r pb-4 transition-all duration-300",
                     {
                         "w-52 px-4": isSidebarOpen,
                         "w-14 px-2": !isSidebarOpen,
@@ -152,71 +211,12 @@ export default function Authenticated({ user, children }) {
                     </Link>
                 </nav>
             </div>
-            <div className="flex grow flex-col">
-                <div className="flex h-16 items-center gap-4 border-b px-6 justify-end">
-                    <div>
-                        <input
-                            className="w-full rounded-md border-0 py-1.5 pl-7 pr-20 ring-1 ring-inset ring-slate-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
-                            type="text"
-                            placeholder="Search"
-                        />
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <div className="border-r px-4 hidden sm:block">
-                            <BellIcon className="h-6 w-6 cursor-pointer text-slate-700" />
-                        </div>
-                        <div className="ml-2 flex items-center justify-center gap-2">
-                            <div>
-                                <Dropdown>
-                                    <Dropdown.Trigger>
-                                        <span className="inline-flex rounded-md">
-                                            <button
-                                                type="button"
-                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                            >
-                                                {user.name}
-
-                                                <svg
-                                                    className="ms-2 -me-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fillRule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clipRule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </Dropdown.Trigger>
-
-                                    <Dropdown.Content>
-                                        <Dropdown.Link
-                                            className="sm:hidden"
-                                            href={route("profile.edit")}
-                                        >
-                                            Settings
-                                        </Dropdown.Link>
-                                        <Dropdown.Link
-                                            href={route("logout")}
-                                            method="post"
-                                            as="button"
-                                        >
-                                            Log Out
-                                        </Dropdown.Link>
-                                    </Dropdown.Content>
-                                </Dropdown>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <BottomNav />
-                <div className="h-[calc(100vh-4rem)] overflow-y-auto pt-5 pb-24 sm:pb-8">
+            <div className="w-full">
+                <div className="h-[100vh] overflow-y-auto pt-24 pb-20 sm:pb-5">
                     {children}
                 </div>
             </div>
+            <BottomNav />
         </div>
     );
 }
