58662c8ef6f8bab607c4f7c0b87614055216d2f3
murad <kh4lidov@mail.ru>
2024-08-16 17:46:56 +0300

    check captcha response

--- app/Http/Requests/Auth/RegisterUserRequest.php
+++ app/Http/Requests/Auth/RegisterUserRequest.php
@@ -87,7 +87,7 @@ class RegisterUserRequest extends FormRequest
             'remoteip' => $ip
         ]);
 
-        $this->dd($response);
+        dd($response);
 
         if (!$response->successful()) {
             $validator->errors()->add('token', 'Что-то пошло не так, попробуйте снова.');
