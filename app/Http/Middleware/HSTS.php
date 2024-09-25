688b81d1a2ad295391e54cd5f55ba49f86f379ce
murad <kh4lidov@mail.ru>
2024-09-25 12:25:35 +0300

    add types

--- app/Http/Middleware/HSTS.php
+++ app/Http/Middleware/HSTS.php
@@ -13,7 +13,7 @@ class HSTS
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
         $response = $next($request);
 
